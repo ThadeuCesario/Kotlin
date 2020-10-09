64e804c0d57cd04d473ee86c5c4aa040e8aa0e9e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-08 23:31:42 -0300

    Problems with inference

---
+++ src/fundamentos/CuidadoInferencia2.kt
@@ -0,0 +1,13 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    var a = 1;
+    var b = 2;
+
+
+    /* Da forma como está o código abaixo, ocorrerá um problema porque não informamos que a variável
+     * é do tipo double.
+     * */
+     //a = 2.3;
+     print(a+b);
+}
