255cc0aad609cf38d55e99b9d604e83f42f94409
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-08 22:59:29 -0300

    Types of comments

--- src/fundamentos/Comentários.kt
+++ src/fundamentos/Comentários.kt
@@ -1,2 +1,19 @@
 package fundamentos
 
+/**
+ * Comentários
+ * KDoc
+ * <3
+ *
+ * @param testando o KDoc!
+ */
+fun main(args: Array<String>) {
+    //Comentário de apenas uma linha
+
+    /* Comentários
+     * em bloco.
+     * Comentários simples... Sem KDoc.
+     */
+
+
+}
