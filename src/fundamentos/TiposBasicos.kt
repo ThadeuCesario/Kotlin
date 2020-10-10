9db0c72ef3adb62a2c47f68d45534c4f43799a32
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 02:17:42 -0300

    Type of numbers

--- src/fundamentos/TiposBasicos.kt
+++ src/fundamentos/TiposBasicos.kt
@@ -1,7 +1,14 @@
 package fundamentos
 
 fun main(args: Array<String>) {
+    /*
+    * Boa prática de programação: Tenha sempre mais constante em seu programa. Portanto sempre parta de constantes.
+    * */
+
     //Tipos numéricos inteiros
     val num1: Byte = 127;
-
+    val num2: Short = 32767;
+    val num3: Int = 2_147_483_647; // É possível realizar essas separaçôes no kotlin.
+    val num4: Long = 9_223_372_036_854_775_807;
+    print(Long.MAX_VALUE);
 }
