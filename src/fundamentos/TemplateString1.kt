6aaf26c92f1d92e68b50d9cc2fc55127c8588897
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 11:22:10 -0300

    Second class - template string

--- src/fundamentos/TemplateString1.kt
+++ src/fundamentos/TemplateString1.kt
@@ -2,5 +2,5 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val aprovados = listOf<String>("João", "Maria", "Pedro");
-    print("O primeiro colocado foi ${aprovados[0]}")
+    print("O primeiro colocado foi ${aprovados[0]}");
 }
---
+++ src/fundamentos/TemplateString2.kt
@@ -0,0 +1,6 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val bomHumor = true;
+    print("Hoje estou de ${if(bomHumor) "feliz" else "triste"}");
+}
