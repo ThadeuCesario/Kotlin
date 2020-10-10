ad0080201e6360ca7d892ae9cf8055e6a24e38a0
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 02:22:56 -0300

    Type of numbers

--- src/fundamentos/TiposBasicos.kt
+++ src/fundamentos/TiposBasicos.kt
@@ -11,4 +11,8 @@ fun main(args: Array<String>) {
     val num3: Int = 2_147_483_647; // É possível realizar essas separaçôes no kotlin.
     val num4: Long = 9_223_372_036_854_775_807;
     print(Long.MAX_VALUE);
+
+    //Tipos numéricos Reais
+    val num5: Float = 3.14F; //Para declararmos uma variável do tipo float, precisamos colocar o F.
+    val num6: Double = 3.14;
 }
