6bef0771b6b912319a65ae50f718246ccc41fa21
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 03:09:26 -0300

    Creating our first function

--- src/fundamentos/FuncaoSemRetorno.kt
+++ src/fundamentos/FuncaoSemRetorno.kt
@@ -1,2 +1,10 @@
 package fundamentos
 
+fun imprimirSoma(a: Int, b: Int){
+    print("função imprimirSoma carregada ");
+    println(a + b);
+}
+
+fun main(args: Array<String>) {
+    imprimirSoma(10, 10);
+}
