d4947ffadc276e1114c2cf990948caf20de36a08
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 13:40:19 -0300

    Using If and Else in Kotlin

---
+++ src/fundamentos/controle/IfElse.kt
@@ -0,0 +1,11 @@
+package fundamentos.controle
+
+fun main(args: Array<String>) {
+    val nota: Double = 5.3
+
+    if(nota >= 7.0) {
+        println("Aprovado 👍");
+    } else {
+        println("Reprovado 👎");
+    }
+}
