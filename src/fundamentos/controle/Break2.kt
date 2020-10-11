bfc64cc2229213016b846bbc7b02e38fadcc8592
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 23:19:07 -0300

    breaking an specific loop

--- src/fundamentos/controle/Break2.kt
+++ src/fundamentos/controle/Break2.kt
@@ -1,9 +1,10 @@
 package fundamentos.controle
 
 fun main(args: Array<String>) {
-    loop@for(i in 1..15){
+    externo@for(i in 1..15){
         for(j in 1..15){
-            if(i == 2 && j == 9) break@loop; //Nunca usar isso haha.
+            if(i == 2 && j == 9) break@externo; //Nunca usar isso haha.
+            println("Valor de i = $i, valor de j = $j");
         }
     }
 }
