d2a2ff198cde4802bfc850ab585427f11c6fc53d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 20:10:13 -0300

    Checking types

--- src/fundamentos/ChecagemDeTipo.kt
+++ src/fundamentos/ChecagemDeTipo.kt
@@ -1,2 +1,13 @@
 package fundamentos
 
+fun main(args: Array<String>) {
+    val valor: Any = "Opa";
+
+    if(valor is String){
+        println("O $valor é do tipo string");
+    } else if (valor !is String){
+        println("O $valor não é do tipo string");
+    } else {
+        println("EKNI -> Elemento Kotlin Não Identificado");
+    }
+}
