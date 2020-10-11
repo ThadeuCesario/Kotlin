5653de7de202dce2be293932482afcfe96900b61
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 19:42:13 -0300

    Ternary operation in kotlin

--- src/fundamentos/operadores/Ternario2.kt
+++ src/fundamentos/operadores/Ternario2.kt
@@ -1,2 +1,7 @@
 package fundamentos.operadores
 
+fun obterResultado(nota: Double): String = if (nota >= 7.0) "Aprovado" else "Reprovado";
+
+fun main(args: Array<String>) {
+    println(obterResultado(5.3));
+}
