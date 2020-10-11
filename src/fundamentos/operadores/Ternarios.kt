c670ec65b7fb2a730d7f3cf8001027c45a7b0677
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 19:32:32 -0300

    ternary operato

--- src/fundamentos/operadores/Ternarios.kt
+++ src/fundamentos/operadores/Ternarios.kt
@@ -1,2 +1,8 @@
 package fundamentos.operadores
 
+fun main(args: Array<String>) {
+    val nota: Double = 9.0;
+    val resultado: String = if(nota >= 7) "Aprovado" else "Reprovado";
+
+    println("Resultado $resultado");
+}
