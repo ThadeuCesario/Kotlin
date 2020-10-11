c69489e17df6574153e59df6cfb70049dd4906e8
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 21:49:54 -0300

    Implemeting a counter using while

---
+++ src/fundamentos/controle/While2.kt
@@ -0,0 +1,10 @@
+package fundamentos.controle
+
+fun main(args: Array<String>) {
+    var contador: Int = 1;
+
+    while(contador <= 10){
+        println("contador = $contador");
+        contador++;
+    }
+}
