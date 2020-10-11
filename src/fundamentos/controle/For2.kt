c84baa2c7e4b8d415998c2135bd993715b9b7c70
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 22:02:49 -0300

    How implemeting for loop in Kotlin - 2

--- src/fundamentos/controle/For2.kt
+++ src/fundamentos/controle/For2.kt
@@ -1,2 +1,11 @@
 package fundamentos.controle
 
+fun main(args: Array<String>) {
+    /*
+    * Veja a sintaxe do código abaixo, temos um laço 'for'.
+    * Porém estamos aplicando um laço decrescente. Por isso temos o downTo.
+    * */
+    for(i in 10 downTo 1){
+        println("i = $i");
+    }
+}
