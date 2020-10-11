cfe6935675c60ce825bb246f49671cc9def64a2a
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 13:20:03 -0300

    Using destructurin in kotlin

--- src/fundamentos/operadores/Destructuring.kt
+++ src/fundamentos/operadores/Destructuring.kt
@@ -1,2 +1,22 @@
 package fundamentos.operadores
 
+data class Carro(val marca: String, val modelo: String);
+
+fun main(args: Array<String>) {
+    /*
+    * Veja abaixo que estamos aplicanto o Destructuring através do Kotlin.
+    * Basicamente criamos uma classe e instaciamos nosso objeto ao mesmo tempo aplicando o destructuring
+    * nos atributos de marca e modelo.
+    * */
+    val (marca, modelo) = Carro("chevrolet", "corsa");
+    println("A marca é $marca");
+    println("O modelo é $modelo");
+
+    val (marido, mulher) = listOf<String>("João", "Maria");
+    println("Nome do marido: $marido");
+    println("Nome da mulher: $mulher");
+
+    val (_, _, terceiroLugar) = listOf<String>("Kimi", "Hamilton", "Alonso");
+    println("Verificando o terceiro lugar: $terceiroLugar");
+
+}
