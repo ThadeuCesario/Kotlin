1d6570cae53900355923811f8b55f678f854431d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 22:17:27 -0300

    How implemeting for loop in Kotlin - 4

--- src/fundamentos/controle/For3.kt
+++ src/fundamentos/controle/For3.kt
@@ -4,4 +4,12 @@ fun main(args: Array<String>) {
     for(i in 0..100 step 5){
         println("Verificando o valor de i => $i");
     }
+
+    for(i in 100 downTo 0 step 5){
+        /*
+        * Veja que dessa forma, estamos aplicando um contador que inicializará em 100, até chegar em 0.
+        * Sendo que o step é de 5 em 5.
+        * */
+        println("Verificando o valor de i => $i");
+    }
 }
