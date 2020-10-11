21d0061ddb70670caddefa7723147bdd6d1aa9c6
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 22:28:57 -0300

    How implemeting for loop in Kotlin - 4

--- src/fundamentos/controle/For4.kt
+++ src/fundamentos/controle/For4.kt
@@ -1,2 +1,8 @@
 package fundamentos.controle
 
+fun main(args: Array<String>) {
+    val alunos = arrayListOf<String>("Thadeu",  "Guilherme", "Clayton", "Fernando", "Philipe");
+    for ((indice, aluno) in alunos.withIndex()){
+        println("Indice $indice - Aluno $aluno");
+    }
+}
