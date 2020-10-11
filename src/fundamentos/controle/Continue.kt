38e406c7fd52e0b31912b1d5810140e5f5b825c6
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 23:39:35 -0300

    Using continue in kotlin

--- src/fundamentos/controle/Continue.kt
+++ src/fundamentos/controle/Continue.kt
@@ -1,2 +1,11 @@
 package fundamentos.controle
 
+fun main(args: Array<String>) {
+    val initialValue: Int = 1;
+    val finalValue: Int = 10;
+    for(value in initialValue..finalValue){
+        if(value == 5) continue;
+        println("Valor $value");
+    }
+    println("End Of File");
+}
