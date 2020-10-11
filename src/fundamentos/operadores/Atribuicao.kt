6a78a662d5739ee9f96bd455cf9b957772ee7bc1
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 12:53:23 -0300

    Operations

--- src/fundamentos/operadores/Atribuicao.kt
+++ src/fundamentos/operadores/Atribuicao.kt
@@ -1,2 +1,23 @@
 package fundamentos.operadores
 
+fun main(args: Array<String>) {
+    var a: Int = 7;
+    var b: Int = 3;
+
+    // Atribuição aditiva
+    b += a; // b = b + a;
+
+    // Atribuição subtrativa
+    b -= 4; // b = b - 4;
+
+    // Atribuição multiplicativa
+    b *= 2; // b = b * 2;
+
+    // Atribuição divisiva
+    b /= 2; // b = b / 2;
+
+    // Atribuição por módulo
+    b %= 2;
+
+    println(b);
+}
