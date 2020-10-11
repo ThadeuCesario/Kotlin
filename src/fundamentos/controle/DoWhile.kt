48c2f22649854120a43c5976a60f6b4f9b4193a7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-10 22:46:38 -0300

    do while in kotlin

--- src/fundamentos/controle/DoWhile.kt
+++ src/fundamentos/controle/DoWhile.kt
@@ -1,2 +1,19 @@
 package fundamentos.controle
 
+fun main(args: Array<String>) {
+    var opcao: Int = -1;
+
+    /*
+    * Como estamos utilizando o Do While, até podemos inicializar nossa opção com o valor -1.
+    * Isso porque obrigatoriamente o bloco DO será executado pelo menos uma vez, portanto será solicitado para
+    * o usuário que insira um número no console.
+    * */
+
+    do {
+        val line = readLine() ?: "0";
+        opcao = line.toIntOrNull() ?: 0;
+        println("Você escolheu a opção $opcao");
+    } while(opcao != -1);
+
+    println("Até a próxima");
+}
