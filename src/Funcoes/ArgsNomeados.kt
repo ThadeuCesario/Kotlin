1830fe764cc04fe8945dd68f0b5ebec5da0d8dda
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 22:47:59 -0300

    Named Parameters

--- src/Funcoes/ArgsNomeados.kt
+++ src/Funcoes/ArgsNomeados.kt
@@ -1,2 +1,14 @@
 package Funcoes
 
+fun relacaoDeTrabalho(chefe: String, funcionario: String): String = "$funcionario é subordinado(a) à $chefe.";
+
+
+/*
+* Veja que um recurso muito interessante do Kotlin, é que podemos passar os parametros de forma nomeada
+* para nossas funções. Isso permite um nível de organizaçaão bem melhor. Além disso, pelo fato de estarmos indicando
+* qual parâmetro que estamos referenciando, podemos até mesmo inverter a ordem. =)
+* */
+fun main(args: Array<String>) {
+    println(relacaoDeTrabalho("João", "Maria"));
+    println(relacaoDeTrabalho(funcionario = "João", chefe = "Maria"));
+}
