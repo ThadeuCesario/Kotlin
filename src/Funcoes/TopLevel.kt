372b4d1a8760599be1ef86235bfbe2e059282867
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 21:58:01 -0300

    Top Level Function

--- src/Funcoes/TopLevel.kt
+++ src/Funcoes/TopLevel.kt
@@ -1,2 +1,11 @@
 package Funcoes
 
+/*
+* Funções TopLevel, são funções que estão fora de classe.
+* */
+
+fun min(a: Int, b: Int): Int = if(a < b) a else b;
+
+fun main(args: Array<String>) {
+    print("O menor valor é ${min(3, 4)}");
+}
