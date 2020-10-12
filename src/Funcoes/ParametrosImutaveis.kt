17ebba6d8846f4c531359d1812540c45065681ca
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-11 22:56:02 -0300

    Immutable parameters

--- src/Funcoes/ParametrosImutaveis.kt
+++ src/Funcoes/ParametrosImutaveis.kt
@@ -1,2 +1,17 @@
 package Funcoes
 
+/*
+* Um parâmetro imutável significa que não podemos mudar a referência para onde este parâmetro aponta.
+* Podemos até mudar os parâmetros internos, porém não podemos mudar a referência.
+*
+* Obs.: Todos os parâmetros de uma função, são constantes. Podemos alterar os valores internos, porém não podemos
+* é realizar uma reatribuição.
+* */
+
+fun incremento(num: Int){
+    //num++; // num = num + 1;
+}
+
+fun main(args: Array<String>) {
+    incremento(5);
+}
