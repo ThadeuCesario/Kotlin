873ab27fe7405d9aad1c9e44e1e2dd924bea3acf
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-13 22:49:22 -0300

    internal kotlin file

--- src/Funcoes/SegundoElementoList.kt
+++ src/Funcoes/SegundoElementoList.kt
@@ -1,2 +1,8 @@
 package Funcoes
 
+fun <E> List<E>.secondOrNull(): E? = if(this.size >= 2) this.get(1) else null;
+
+fun main(args: Array<String>) {
+    val list = listOf<String>("João", "Maria", "Pedro", "Karina", "Thadeu");
+    println(list.secondOrNull()); // "Maria"
+}
