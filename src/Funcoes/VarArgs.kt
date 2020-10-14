5857bff8b172a749e09af547c364f677eb29ab39
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-13 23:45:31 -0300

    kotlin internal files

--- src/Funcoes/VarArgs.kt
+++ src/Funcoes/VarArgs.kt
@@ -1,2 +1,20 @@
 package Funcoes
 
+/*
+* Quando utilizamos o vararg, estamos dizendo que passaremos um conjunto de argumentos variáveis.
+* Sendo que esse conjunto de inteiros será convertido para um array de inteiros.
+*
+* Um ponto importante de mencionar é que se quisermos passar um paraâmetro, após
+* o varargs, precisamos deixar explícito na passagem do paraâmetro o seu nome.
+* veja por exemplo o parâmetro 'a' que está após o vararg.
+* */
+fun ordenar(vararg numeros: Int, a: Int): IntArray{
+    println(a);
+    return numeros.sortedArray(); //Retornará o array ordenado.
+}
+
+fun main(args: Array<String>) {
+    for(n in ordenar(1,5,3,8,0,763,332, a = 12)){
+        print("$n ");
+    }
+}
